363b58a6766543b8a7f568a58b3629987bb0ddef
IslamAhmed1092 <eslam1092@hotmail.com>
2022-03-30 17:18:06 +0200

    solve rotting oranges problem using bfs

--- rotting_oranges.cpp
+++ rotting_oranges.cpp
@@ -1,42 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(vector<vector<int>>& grid, int i, int j, int time, vector<vector<int>>& times) {
-    int m = grid.size(), n = grid[0].size();
+// void dfs(vector<vector<int>>& grid, int i, int j, int time, vector<vector<int>>& times) {
+//     int m = grid.size(), n = grid[0].size();
     
-    int neighbors[4][2] = {{i,j+1}, {i+1, j}, {i, j-1}, {i-1,j}};
+//     int neighbors[4][2] = {{i,j+1}, {i+1, j}, {i, j-1}, {i-1,j}};
 
-    for (int k = 0; k < 4; k++) {
-        int r = neighbors[k][0], c = neighbors[k][1];
-        if(r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 1 && time+1 < times[r][c]){
-            times[r][c] = time+1;
-            dfs(grid, r, c, time+1, times);
-        }
-    }
+//     for (int k = 0; k < 4; k++) {
+//         int r = neighbors[k][0], c = neighbors[k][1];
+//         if(r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 1 && time+1 < times[r][c]){
+//             times[r][c] = time+1;
+//             dfs(grid, r, c, time+1, times);
+//         }
+//     }
     
-}
+// }
 
 int orangesRotting(vector<vector<int>>& grid) {
     int m = grid.size(), n = grid[0].size();
-    vector<vector<int>> times(m, vector<int>(n, INT32_MAX));
+    int minutes = 0;
+
+    queue<pair<int,int>> q;
+
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             if(grid[i][j] == 2)
-                dfs(grid, i, j, 0, times);
+                q.push(make_pair(i, j));
         }
     }
 
-    int maxTime = 0;
+    q.push(make_pair(-1, -1));
+
+
+    while (!q.empty()) {
+        
+        pair<int, int> current = q.front();        
+        
+        while (!(current.first == -1 && current.second == -1)) {
+            int i = current.first, j = current.second; 
+            int neighbors[4][2] = {{i,j+1}, {i+1, j}, {i, j-1}, {i-1,j}};
+            for (int k = 0; k < 4; k++) {
+                int r = neighbors[k][0], c = neighbors[k][1];
+                if(r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == 1){
+                    grid[r][c] = 2;
+                    q.push(make_pair(r,c));
+                }
+            }
+
+            q.pop();
+            current = q.front();
+        }
+        
+        q.pop();
+
+        if(!q.empty()) {
+            minutes++;
+            q.push(make_pair(-1, -1));
+        }
+    }
+    
+
+
+
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             if(grid[i][j] == 1) { 
-                if(times[i][j] == INT32_MAX) return -1;
-                maxTime = max(maxTime, times[i][j]);           
+                return -1;           
             }
         }
     }
 
-    return maxTime;
+    return minutes;
 }
 
 int main() {
