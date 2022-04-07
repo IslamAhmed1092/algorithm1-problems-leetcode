1bb0a9dac7747d9cb1c5bf84640e395016cadcc8
IslamAhmed1092 <eslam1092@hotmail.com>
2022-04-07 16:36:25 +0200

    remove comments

--- number_of_1_bits.cpp
+++ number_of_1_bits.cpp
@@ -1,12 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// #define B2(n) n, n + 1, n + 1, n + 2
-// #define B4(n) B2(n) \, B2(n + 1), B2(n + 1), B2(n + 2)
-// #define B6(n) B4(n) \, B4(n + 1), B4(n + 1), B4(n + 2)
- 
-// unsigned int lookuptable[256] = { B6(0), B6(1), B6(1), B6(2) };
-
 int hammingWeight(uint32_t n) {
     int count = 0;
     while(n) {
